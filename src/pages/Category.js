53d4082e06058fd99681d1fd493f16c7f8cb216d
mohamed <mohamed.mohamed.yossef@gmail.com>
2021-07-29 16:54:25 +0200

    first commit

---
+++ src/pages/Category.js
@@ -0,0 +1,45 @@
+import { useParams,Link } from "react-router-dom"
+import { useQuery, gql } from '@apollo/client'
+
+const CATEGORY = gql`
+  query GetCategories($id:ID!) {
+    category(id:$id) {
+      name,
+      id
+	  reviews{
+		  title,
+		  body,
+		  rating,
+		  id,
+		  categories{
+			  name,
+			  id
+		  }
+	  }
+    }
+  }
+`
+export default function Category() {
+	const { id } = useParams()
+  const { loading, error, data } = useQuery(CATEGORY,{variables:{id:id}})
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+  return (
+    <div>
+      <h2>{ data.category.name } Games</h2>
+      {data.category.reviews.map(review => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.rating}</div>
+          <h2>{review.title}</h2>
+
+          {review.categories.map(c => (
+            <small key={c.id}>{c.name}</small>
+          ))}
+          
+          <p>{review.body.substring(0, 200)}...</p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
+    </div>
+  )
+}
